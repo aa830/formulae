3ac312e550e72f879c29cf10f5e6b332d58d6f6a
aa830 <84448225+aa830@users.noreply.github.com>
2024-11-17 14:13:22 +0800

    Added donut formulae file

---
+++ donut.rb
@@ -0,0 +1,23 @@
+class Donut < Formula
+  desc "The original donut.c, but better!"
+  homepage "https://github.com/aa830/donut.c"
+  url "https://github.com/aa830/homebrew-donut.c/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "eb7ab5243ff3b3842ffc68e4696b29ec4595983f65f7f764cb1c10d765584827"
+  version "0.1.2"
+
+  def install
+    # Compile the project using `make`
+    system "make"  # Ensure a `Makefile` exists in the project root directory
+
+    # Install the compiled binary to Homebrew's bin directory
+    bin.install "donut"
+
+    # Install the man page
+    man1.install "man/man1/donut.1"
+  end
+
+  test do
+    # Simple test to verify the installation
+    system "#{bin}/donut", "--help"
+  end
+end
