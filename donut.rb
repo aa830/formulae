172cd308e2c3aef29c8116e884d33c4cc39f4880
aa830 <84448225+aa830@users.noreply.github.com>
2024-11-18 10:44:04 +0800

    Updated formula

--- donut.rb
+++ donut.rb
@@ -2,7 +2,7 @@ class Donut < Formula
   desc "The original donut.c, but better!"
   homepage "https://github.com/aa830/donut.c"
   url "https://github.com/aa830/homebrew-donut.c/archive/refs/tags/0.1.5.tar.gz"
-  sha256 "4ad14dcc96cbd387424ecdc80c0de26691043cd85ac39fe8308ec80973775d9b"
+  sha256 "1a4db4d037f8ebfc0c604d8f9abb619a673efb3289baf840a788d886bfc0cdea"
   version "0.1.5"
 
   def install
